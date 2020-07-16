c20916d49bb25f8a9d07dc79576398e2a3166362
kenenwobodo <nwobodokenechukwu2@gmail.com>
2020-07-16 13:56:31 +0100

    messages and commit

--- index.php
+++ index.php
@@ -181,7 +181,7 @@ if(isset($_POST['submit']))
 
 <!--coming out soon section-->
  <section id="coming-out-section">
-    <div class="blue-container d-flex justify-content-center">
+    <div class="container d-flex align-items-center">
       <div>
          <h2 class="text-center"><span>AgedPrompt</span>will be coming to devices soon</h2>
           <p>Sign up to join the waitlist</p>
